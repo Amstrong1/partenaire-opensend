98fb0f411f6f9e647338b242fa72b0c0d9fdb512
amstrong1 <lewisamstrongh@gmail.com>
2024-03-04 18:39:17 +0100

    menu

--- resources/views/layouts/nav-top.blade.php
+++ resources/views/layouts/nav-top.blade.php
@@ -21,34 +21,7 @@
                 <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 13.5 12 21m0 0-7.5-7.5M12 21V3" />
             </svg>
             &nbsp;
-            Dépot
-        </x-nav-link>
-    </li>
-
-    <li class="mx-1 static" data-te-nav-item-ref>
-        <x-nav-link
-            class="dark:text-white text-black flex items-center whitespace-nowrap py-1 transition duration-150 ease-in-out hover:text-black focus:text-black dark:hover:text-white dark:focus:text-white"
-            href="#" active="" data-te-ripple-init data-te-ripple-color="light">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                stroke="currentColor" class="w-6 h-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 13.5 12 21m0 0-7.5-7.5M12 21V3" />
-            </svg>
-            &nbsp;
-            Dépot Interac
-        </x-nav-link>
-    </li>
-
-    <li class="mx-1 static" data-te-nav-item-ref>
-        <x-nav-link
-            class="dark:text-white text-black flex items-center whitespace-nowrap py-1 transition duration-150 ease-in-out hover:text-black focus:text-black dark:hover:text-white dark:focus:text-white"
-            href="#" active="" data-te-ripple-init data-te-ripple-color="light">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                stroke="currentColor" class="w-6 h-6">
-                <path stroke-linecap="round" stroke-linejoin="round"
-                    d="M7.5 21 3 16.5m0 0L7.5 12M3 16.5h13.5m0-13.5L21 7.5m0 0L16.5 12M21 7.5H7.5" />
-            </svg>
-            &nbsp;
-            Transfert
+            Historique Dépot
         </x-nav-link>
     </li>
 
@@ -61,20 +34,7 @@
                 <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 10.5 12 3m0 0 7.5 7.5M12 3v18" />
             </svg>
             &nbsp;
-            Retrait
-        </x-nav-link>
-    </li>
-
-    <li class="mx-1 static" data-te-nav-item-ref>
-        <x-nav-link
-            class="dark:text-white text-black flex items-center whitespace-nowrap py-1 transition duration-150 ease-in-out hover:text-black focus:text-black dark:hover:text-white dark:focus:text-white"
-            href="#" active="" data-te-ripple-init data-te-ripple-color="light">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                stroke="currentColor" class="w-6 h-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 10.5 12 3m0 0 7.5 7.5M12 3v18" />
-            </svg>
-            &nbsp;
-            Retrait Interac
+            Historique Retrait
         </x-nav-link>
     </li>
 
